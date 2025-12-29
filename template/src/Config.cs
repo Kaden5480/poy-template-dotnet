433b58b64daf511c08245dcd811970ff01bd2bc4
Kaden5480 <kaden5480@protonmail.com>
2025-12-29 19:29:44 +0000

    Add config

---
+++ template/src/Config.cs
@@ -0,0 +1,33 @@
+using BepInEx.Configuration;
+#if (modmenu)
+using ModMenu.Config
+#endif
+
+namespace TemplateMod {
+    /**
+     * <summary>
+     * Holds the config for this mod.
+     * </summary>
+     */
+    internal class Config {
+        // Example config entry
+#if (modmenu)
+        [Field("Enabled")]
+#endif
+        internal ConfigEntry<bool> enabled { get; private set; }
+
+        /**
+         * <summary>
+         * Initializes the config by binding to the
+         * provided `ConfigFile`.
+         * </summary>
+         * <param name="configFile">The config file to bind to</param>
+         */
+        internal static void Init(ConfigFile configFile) {
+            enabled = configFile.Bind(
+                "General", "enabled", true,
+                "Whether something is enabled."
+            );
+        }
+    }
+}
